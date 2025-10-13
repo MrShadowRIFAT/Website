5d2dfd15534efb01ff8e54c226348f55043df28e
Aliul Hassan Olee <aliul.21024008@bsmraau.edu.bd>
2025-10-14 01:05:19 +0600

    Delete mailer directory

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from Aliul Hassan Olee — Portfolio"; // letter subject
-	$to = 'aliul.hassan.olee.aaub@gmail.com'; // Recipient E-mail (updated)
-	$from = 'no-reply@aliul-portfolio.local'; // Sender e-mail (adjust in production)
-	$fromName = 'Aliul Hassan Olee — Portfolio Website'; // Brand name
-	$charset = 'UTF-8';
-?>
